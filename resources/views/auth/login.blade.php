7134f948d6d43630e15df51467d1f3f862ce8d72
Koen Hamstra <koen@Koens-MacBook-Pro.local>
2021-05-18 13:51:58 +0200

    login register html

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
@@ -54,3 +57,5 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+</body>
+</html>
